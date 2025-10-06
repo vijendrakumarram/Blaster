7c1430a0c81983112783d02b0508d2de2ab080b9
Vijendra <vijendrakumarram@gmail.com>
2025-10-06 10:17:49 +0530

    Predict Projectile Path and Post Edit Change Property

--- Source/Blaster/Weapon/Projectile.h
+++ Source/Blaster/Weapon/Projectile.h
@@ -17,6 +17,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void Destroyed() override;
 
+	/**
+	* Used with server-side rewind
+	*/
+	bool bUseServerSideRewind = false;
+	FVector_NetQuantize TraceStart;
+	FVector_NetQuantize100 InitialVelocity;
+
+	UPROPERTY(EditAnywhere)
+	float InitialSpeed = 15000;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
