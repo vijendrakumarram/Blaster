bd024a514faaa57b56795af7896ece2922a190e2
Vijendra <vijendrakumarram@gmail.com>
2025-10-21 11:23:51 +0530

    Head Shots

--- Source/Blaster/Weapon/Weapon.h
+++ Source/Blaster/Weapon/Weapon.h
@@ -133,7 +133,10 @@ protected:
 	float SphereRadius = 75.f;
 
 	UPROPERTY(EditAnywhere)
-	float Damage = 20.f;
+	float Damage = 20.f;	
+	
+	UPROPERTY(EditAnywhere)
+	float HeadShotDamage = 40.f;
 
 	UPROPERTY(Replicated, EditAnywhere)
 	bool bUseServerSideRewind = false;
@@ -200,4 +203,5 @@ public:
 	FORCEINLINE int32 GetAmmo() const { return Ammo; }
 	FORCEINLINE int32 GetMagCapacity() const { return MagCapacity; }
 	FORCEINLINE float GetDamage() const { return Damage; }
+	FORCEINLINE float GetHeadShotDamage() const { return HeadShotDamage; }
 };
