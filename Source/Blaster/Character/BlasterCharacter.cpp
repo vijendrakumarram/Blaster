cc159a774c4651839e0d85c1bbf9d313b02d5e93
Vijendra <vijendrakumarram@gmail.com>
2025-10-22 16:49:36 +0530

    Head Shots Server-Side Rewind

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -564,8 +564,8 @@ void ABlasterCharacter::ReceiveDamage(AActor* DamagedActor, float Damage, const 
 		}
 		else
 		{
-			Shield = 0.f;
 			DamageToHealth = FMath::Clamp(DamageToHealth - Shield, 0.f, Damage);
+			Shield = 0.f;
 		}
 	}
 
