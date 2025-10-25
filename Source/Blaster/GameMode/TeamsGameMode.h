b0e2b508ab6de8566bcff6b73232c5e8b2d178f8
Vijendra <vijendrakumarram@gmail.com>
2025-10-25 11:02:11 +0530

    Updating Team Scores

--- Source/Blaster/GameMode/TeamsGameMode.h
+++ Source/Blaster/GameMode/TeamsGameMode.h
@@ -19,6 +19,7 @@ public:
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 	virtual void Logout(AController* Exiting) override;
 	virtual float CalculateDamage(AController* Attacker, AController* Victim, float BaseDamage) override;
+	virtual void PlayerEliminated(class ABlasterCharacter* ElimmedCharacter, class ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController) override;
 protected:
 	virtual void HandleMatchHasStarted() override;
 };
