08380cf2d7bc89ab189fa525aec079979babd3bc
Vijendra <vijendrakumarram@gmail.com>
2025-10-26 09:48:55 +0530

    Holding the Flag

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -1052,3 +1052,9 @@ bool ABlasterCharacter::IsLocallyReloading()
 	if (Combat == nullptr) return false;
 	return Combat->bLocallyReloading;
 }
+
+bool ABlasterCharacter::IsHoldingTheFlag() const
+{
+	if (Combat == nullptr) return false;
+	return Combat->bHoldingTheFlag;
+}
--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -381,4 +381,5 @@ public:
 	FORCEINLINE UBuffComponent* GetBuff() const { return Buff; }
 	bool IsLocallyReloading();
 	FORCEINLINE ULagCompensationComponent* GetLagCompensation() const { return LagCompensation; }
+	FORCEINLINE bool IsHoldingTheFlag() const;
 };
